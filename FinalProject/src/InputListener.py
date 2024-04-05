9657ffc62b9267959d73969b9d193266bcf1978c
swj20010308 <swj20010308@outlook.com>
2024-04-05 18:44:37 -0400

    inputListener is not tested yet because erpy is not available. See if the messages are correct.

--- FinalProject/src/InputListener.py
+++ FinalProject/src/InputListener.py
@@ -1,14 +1,18 @@
-import pygame as py:
+import pygame as py
+import os
+from erpy import stdio_port_connection # TODO: pip install erpy currently 
+                                       # doesn't work
+
 class InputListener:
     def __init__(self):
         pass 
 
 
-    def checkAndSendInput(self):
+    def checkAndSendInput(self): # TODO: py.get()? or py.event.get()
         for event in py.get():
             inputValue = map()
             if event == py.QUIT:
-                inputValue["QUIT"] = None 
+                inputValue["QUIT"] = "QUIT" 
             elif event == py.KEYDOWN:
                 inputValue["KEY"] = event.key
             
@@ -16,5 +20,32 @@ class InputListener:
 
     
     def sendInput(self, inputValue : map):
+        """Message format: PID, command"""
         # TODO: Use the erlang channel to send the (string, value) map over
-        pass 
+        # TODO: Need testing if anyone is able to install erpy
+        _, port = stdio_port_connection()
+        port.send(self.__format_message(inputValue))
+        
+    def __map_key_to_direction(self, key):
+        """Map Pygame key events to direction strings."""
+        match key:
+            case py.K_UP:
+                return 'up'
+            case py.K_DOWN:
+                return 'down'
+            case py.K_LEFT:
+                return 'left'
+            case py.K_RIGHT:
+                return 'right'
+            case _:
+                return None  # Default case if no direction is matched
+
+    
+    def __format_message(self, inputValue: map):
+        """Format the movement message according to specified pattern."""
+        if "QUIT" in inputValue:
+            return os.getpid(), "quit"
+        elif "KEY" in inputValue:
+            return os.getpgid(), self.__map_key_to_direction(inputValue["KEY"])
+        return None
+        
