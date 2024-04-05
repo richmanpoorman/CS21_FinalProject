284d7af2fcc48292ff8613149ebf11e3bea3e1a6
TKA02 <timiabiola@protonmail.com>
2024-04-05 19:34:17 -0400

    Update Display.py

--- FinalProject/src/Display.py
+++ FinalProject/src/Display.py
@@ -1,12 +1,46 @@
 from Board import Board 
+import pygame as py
+import sys
+import GameObject # just for testing
+import random
 
 class Display:
-    def __init__(self):
-        self.board = Board() 
+    def __init__(self,dimension: tuple[int, int] = (10, 12)):
+        self.board = Board(dimension)
+        self.dim = dimension
+        self.window = py.display.set_mode(self.dim)
 
     def updateDisplay(self) -> None:
-        pass 
+        visual = self.board.getBoard()
+        rows, cols = self.board.size
+
+        for r in range(rows):
+            for c in range(cols):
+                obj = visual[r][c]
+                suf = obj.getSurface()
+                self.window.blit(suf, (300, 250))
+
+                py.display.update()
+        
 
     def receiveUpdate(self, update : map) -> None:
         pass 
     
+
+
+
+###################
+#
+# Testing Fuctions
+#
+###################
+
+test_display = Display((800, 600))
+obj1 = GameObject((200,100))
+obj2 = GameObject((400, 100))
+test_display.board.addObject(obj1, (random.randint(0, 800)
+                                    , random.randint(0,600)))
+test_display.board.addObject(obj2, (random.randint(0, 800)
+                                    , random.randint(0,600)))
+
+test_display.updateDisplay()
