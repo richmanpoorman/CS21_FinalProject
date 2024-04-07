964468b0ef6e252a8df6b8a098c4a638e5eb4cd9
TKA02 <timiabiola@protonmail.com>
2024-04-07 14:51:27 -0400

    Implemented updateDisplay
    The code works for 2-D numpy array, has not been tested using a Board object. Also need to figure out the color and size of each type of Gameobject

--- FinalProject/src/Display.py
+++ FinalProject/src/Display.py
@@ -1,29 +1,69 @@
 from Board import Board 
 import pygame as py
 import sys
-import GameObject # just for testing
+from GameObject import GameObject # just for testing
 import random
+import numpy as np
 
 class Display:
     def __init__(self,dimension: tuple[int, int] = (10, 12)):
-        self.board = Board(dimension)
-        self.dim = dimension
-        self.window = py.display.set_mode(self.dim)
+        '''
+            Name    : init
+            Params  : (dimension) tuple[int, int] := optional parameter that has the 
+                      width and height of the board
+            Purpose : initalize specific class variables
+            Return  : N/A
+        '''
 
-    def updateDisplay(self) -> None:
-        visual = self.board.getBoard()
-        rows, cols = self.board.size
+        # NEED TO CHANGE WHEN BOARD IS TESTED
+        # self.board = Board(dimension)
+        self.board = np.empty(dimension, dtype=GameObject)
+        self.dim = dimension
+        #(self.window) is the display surface that is shown on the screen 
+        self.window = py.display.set_mode(self.dim)
+        self.window.fill((0,0,0)) # initalize the screen to be blank
+
+    def __updateDisplay(self) -> None:
+
+        '''
+            Name    : __updateDisplay
+            Params  : N/A
+            Purpose : Update the display window with all the gameObjects in the board
+            Return  : N/A
+        '''
+
+        rows, cols = self.dim
 
         for r in range(rows):
             for c in range(cols):
-                obj = visual[r][c]
+                # NEED TO CHANGE WHEN BOARD IS TESTED
+                # obj = self.board.getAt((r, c))
+                obj = self.board[r][c]
                 suf = obj.getSurface()
-                self.window.blit(suf, (300, 250))
+                # MUST UPDATE WHAT SIZE EACH GAMEOBJECT SHOULD BE
+                self.window.blit(suf, (r * 10, c * 10))
+        
+    
+        py.display.update()
 
-                py.display.update()
+        
+    def display_window(self):
+        '''
+            Name    : display_window
+            Params  : N/A
+            Purpose : To display the self.window to the screen 
+            Return  : N/A
+        '''
+        run = True
+        while run:
+            for event in py.event.get():
+                if event.type == py.QUIT:
+                    run = False
+
+            self.__updateDisplay()
         
 
-    def receiveUpdate(self, update : map) -> None:
+    def receiveUpdate(self, update : dict) -> None:
         pass 
     
 
@@ -31,16 +71,33 @@ class Display:
 
 ###################
 #
-# Testing Fuctions
+# Testing Stuff
 #
 ###################
 
-test_display = Display((800, 600))
-obj1 = GameObject((200,100))
-obj2 = GameObject((400, 100))
-test_display.board.addObject(obj1, (random.randint(0, 800)
-                                    , random.randint(0,600)))
-test_display.board.addObject(obj2, (random.randint(0, 800)
-                                    , random.randint(0,600)))
+py.init()
 
-test_display.updateDisplay()
+# Create a Display object
+ROWS = 500
+COLS = 500
+dis = Display((COLS, ROWS))
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+
+# Create GameObjects and add them to the display
+for c in range(COLS):
+    for r in range(ROWS):
+        surf = py.Surface((10, 10))
+        surf.fill(GREEN)
+        if (c % 2) == 0:
+            surf.fill(RED)
+        dis.board[c][r] = GameObject(surf)
+
+dis.display_window()
+
+py.quit()
+
