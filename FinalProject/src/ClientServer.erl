2784993372682b2fd34cbb538b03860d7ce11212
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2024-04-09 19:04:51 -0400

    change to gameobject

--- FinalProject/src/ClientServer.erl
+++ FinalProject/src/ClientServer.erl
@@ -1,12 +1,19 @@
+-module(client_server).
 -behaviour(gen_server).
 
 %% API
 -export([start/1, stop/1, start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--record(state, {dummy}).
-start(Name) ->
-    _sup:start_child(Name).
+-record(state, {users}).
 
+
+%%% CLIENT FUNCTIONS %%% 
+join_game() -> ok. 
+
+send_input(Server, Input) -> 
+    gen_server:cast(Server, {input, Input}).
+
+%%% SERVER FUNCTIONS %%%
 stop(Name) ->
     gen_server:call(Name, stop).
 
@@ -14,22 +21,39 @@ start_link(Name) ->
     gen_server:start_link({local, Name}, ?MODULE, [], []).
 
 init(_Args) ->
-    {ok, #state{dummy=1}}.
+    {ok, #state{users = []}}.
 
+%%% BROADCAST %%%
+broadcastAll([Info | Rest], Users) -> 
+    broadcast(Info, Users),
+    broadcastAll(Rest, Users);
+broadcastAll([], Users) -> 
+    ok.  
+
+broadcast(Information, [User | Users]) -> 
+    User ! Information,
+    broadcast(Information, Users);
+broadcast(Information, []) -> 
+    ok. 
+
+%%% HANDLE CALL %%%
 handle_call(stop, _From, State) ->
     {stop, normal, stopped, State};
-
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
+%%% HANDLE CAST %%%
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+%%% HANDLE OTHER %%%
 handle_info(_Info, State) ->
     {noreply, State}.
 
+%%% END %%%
 terminate(_Reason, _State) ->
     ok.
 
+%%% CODE CHANGE %%%
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
--- FinalProject/src/GameObject.py
+++ FinalProject/src/GameObject.py
@@ -3,7 +3,6 @@ from pygame import Surface
 class GameObject:
     def __init__(self, surface : Surface | None):
         self.setSurface(surface)
-        self.onTopOf = None
 
     def getSurface(self) -> Surface | None: 
         return self.surface
