22b669e7f7fd2d212320e64c9a8d9a0d7c56f210
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2024-04-21 22:55:55 -0400

    wall image

--- FinalProject/src/Wall.py
+++ FinalProject/src/Wall.py
@@ -4,14 +4,17 @@ from os import environ
 environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 from pygame import Surface 
 
+import pygame as py
+
 class Wall(GameObject):
+    path = "./images/wall.png"
+    wallImage = py.image.load(path)
     def __init__(self):
         super().__init__(None)
 
     def getSurface(self) -> Surface | None:
-        surface = Surface((200, 200))
-        surface.fill((100, 200, 0))
-        return surface
+        
+        return self.wallImage
     
     def pack(self) -> tuple[str, dict[str, str]]:
         return ("wall", dict())
