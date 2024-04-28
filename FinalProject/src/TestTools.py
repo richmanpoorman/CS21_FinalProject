c70f845a994428268e6aecdf0c9bff0d9418c795
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2024-04-28 17:59:31 -0400

    README, cleanup, and some headers

--- FinalProject/src/TestTools.py
+++ FinalProject/src/TestTools.py
@@ -3,12 +3,22 @@ TEST_FILE = "Test.txt"
 SHOULD_PRINT = False
 
 def outputInit():
+    '''
+        Params  : (None)
+        Purpose : Clears out the test file
+        Return  : (None)
+    '''
     if not SHOULD_PRINT:
         return
     with open(TEST_FILE, "w") as file:
         file.write("")
 
 def outputLn(message : str):
+    '''
+        Params  : (str) message := The message to print to the log file
+        Purpose : Print a line to the test file
+        Return  : None
+    '''
     if not SHOULD_PRINT:
         return
     with open(TEST_FILE, "a") as file:
--- FinalProject/src/Wall.py
+++ FinalProject/src/Wall.py
@@ -10,18 +10,41 @@ class Wall(GameObject):
     path = "./images/wall.png"
     wallImage = py.image.load(path)
     def __init__(self):
+        '''
+            Params  : (None)
+            Purpose : Creates a wall
+        '''
         super().__init__(None)
 
     @staticmethod 
     def convertImage():
+        '''
+            Params  : (None)
+            Purpose : Converts the surface into an image which is easier to render
+            Return  : (None)
+        '''
         Wall.wallImage = Wall.wallImage.convert()
 
     def getSurface(self) -> Surface | None:
-        
+        '''
+            Params  : (None)
+            Purpose : Gets the image for the object to render
+            Return  : (Surface) The display of the object
+        '''
         return self.wallImage
     
     def pack(self) -> tuple[str, dict[str, str]]:
+        '''
+            Params  : (None)
+            Purpose : Packs the object to cross the erlang channel
+            Return  : (["wall", Message]) The packed object
+        '''
         return ("wall", dict())
     
-    def unpack(self, data : dict):
+    def unpack(self, info : dict):
+        '''
+            Params  : (dict) info := The data from the object
+            Purpose : Unpacks the object from the erlang channel
+            Return  : (self) The object with all the information
+        '''
         return self
